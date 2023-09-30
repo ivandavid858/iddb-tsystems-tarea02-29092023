b8ff18733d2fcc37a02cca52aad7ad26eb034764
ivandavid858 <ivandavid858@gmail.com>
2023-09-30 11:39:05 +0200

    Añadido el código del Test1

--- Tarea02/src/Test1.java
+++ Tarea02/src/Test1.java
@@ -2,7 +2,18 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Integer num1 = new Integer(125);
+		Integer num2 = new Integer(20);
+		
+		int n1 = num1.intValue();
+		int n2 = num2.intValue();
+		System.out.println("Suma de "+n1+" y "+n2+" vale " + (n1+n2));
+		if(n1+n2 > 30)
+			n1++;
+		else
+			n1--;
+		System.out.println(n1);
 
 	}
 
